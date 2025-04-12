559462f1de583b76b149531e676ba862f6825ffe
nemanja2424 <njakovlje@gmail.com>
2025-04-12 04:23:59 +0200

    hero gotov

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import Button2 from "./Button2";
+import VideoWithThumbnail from "./VideoWithThumbnail";
 import Link from "next/link";
 
 const Hero = () => {
@@ -25,7 +26,9 @@ const Hero = () => {
                     </div>
                 </div>
             </div>
-
+            <div>
+                <VideoWithThumbnail />
+            </div>
         </div>
     </div>
   );
---
+++ src/components/VideoWithThumbnail.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import style from './Buttons.module.css'
+
+const VideoWithThumbnail = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const playVideo = () => {
+    setIsPlaying(true);
+  };
+
+  return (
+    <div style={{ position: 'relative', width: '100%', height: 'auto' }}>
+      {!isPlaying ? (
+        <div style={{ position: 'relative', width: '100%' }}>
+          <Image 
+            className='video'
+            src={"/images/Thumbnail.jpg"}
+            alt="Thumbnail" 
+            width={897} 
+            height={505} 
+          />
+          <button className={style.playBtn} onClick={playVideo}>
+            <Image src={"/images/play.svg"} alt='play' width={88} height={88} />
+          </button>
+        </div>
+      ) : (
+        <video 
+          controls
+          autoPlay
+          style={{ borderRadius: '20px', maxWidth: '897px', maxHeight:'505px' }}
+        >
+            <source src="/videos/video.webm" type="video/webm" />
+            Your browser does not support the video tag.
+        </video>
+      )}
+    </div>
+  );
+};
+
+export default VideoWithThumbnail;
